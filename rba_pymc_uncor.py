546717e78f70ef89b8c5c299ead2a07c0930ade3
Harrison Curtis <harrison curtis>
2025-01-08 14:56:36 +0000

    add plots

--- rba_pymc_uncor.py
+++ rba_pymc_uncor.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import pymc as pm
 import numpy as np
 import bambi as bmb
 import pytensor.tensor as pt
+import arviz as az
 import time
 import os
 
@@ -10,22 +12,21 @@ import os
 FILE_PATH = "https://raw.githubusercontent.com/HPCurtis/Datasets/refs/heads/main/rba.csv"
 df = pd.read_csv(FILE_PATH)
 
-# Covnert the strings to unique integers. 
-df["roi_int"] = pd.factorize(df["ROI"])[0]
-df["subject_int"] = pd.factorize(df["subject"])[0]
+subject, subjects_id = np.unique(df["subject"], return_inverse=True)
+ROI, ROI_id = np.unique(df["ROI"], return_inverse=True)
 
 # Specify variables for pymc model.
 y = df.y
 N_ROI = len(np.unique(df.ROI))
 N_subj = len(np.unique(df.subject))
-ROI =  df.roi_int.values
-subj = df.subject_int.values
+
 Xc = (df.x.values - np.mean(df.x.values))
 X = df.x.values
 J = 2
 
+coords = {"ROI": ROI, "subject": subject, "effect": ["intercept", "slope"]}
 # RBA model follwing the bambi syntax y ~ x + (1|subject) + (x|ROI) but with correlated slopes"
-with pm.Model() as model:
+with pm.Model(coords=coords) as model:
 
     # Intercept (alpha) and beta (fixed) distribution
     alpha = pm.Normal("alpha", mu=0.17, sigma=4.66)
@@ -34,21 +35,21 @@ with pm.Model() as model:
 
     # Subjct random intercept.
     tau_u = pm.HalfNormal("tau_u", sigma=4.66)
-    z_u = pm.Normal('z_u', mu=0, sigma=1, shape=N_subj)
+    z_u = pm.Normal('z_u', mu=0, sigma=1, dims="subject")
     u = pm.Deterministic("u", z_u * tau_u)
 
     # Randomintecept and slope correlated.
     z_u2 = pm.Normal('z_u2', 0., 1., shape=(N_ROI, J))
       
     tau_u2 = pm.HalfNormal("tau_u2", sigma=4.66, shape= J)
-    u2_int = pm.Deterministic("u2_int", z_u2[:,0] * tau_u2[0])
-    u2_slope = pm.Deterministic("u2_slope", z_u2[:,1] * tau_u2[1])
+    u2_int = pm.Deterministic("u2_int", z_u2[:,0] * tau_u2[0], dims="ROI")
+    u2_slope = pm.Deterministic("u2_slope", z_u2[:,1] * tau_u2[1], dims="ROI")
 
     # Likelihood
-    mu = alpha + beta * Xc + u[subj] + u2_int[ROI] + X * u2_slope[ROI]
+    mu = alpha + beta * Xc + u[subjects_id] + u2_int[ROI_id] + X * u2_slope[ROI_id]
     y = pm.Normal('y', mu = mu, sigma = sigma, observed=y)
     
-if not os.path.exists("model_graph_pymc_uncor.png"):
+if not os.path.exists("vis/model_graph_pymc_uncor.png"):
     fig = pm.model_to_graphviz(model)
     fig.render("model_graph_pymc_uncor", format="png")
     
@@ -56,10 +57,17 @@ if not os.path.exists("model_graph_pymc_uncor.png"):
 start_time = time.time()
 
 with model:
-    # Fit nutpie model for fastest cpu performance.
-    pm.sample(nuts_sampler="nutpie", draws=1000, tune=1000, 
-              chains=4, cores=4, target_accept=0.8)
+    # Fit nutpie model for fastest cpu performance without correlation.
+    fit = pm.sample(nuts_sampler="nutpie", draws=1000, tune=2000, 
+              chains=4, cores=4, target_accept=0.95)
 
 end_time = time.time()
  
 print(f"Execution time: {end_time - start_time} seconds")
+
+print(az.summary(fit, var_names = ["alpha", "beta", "sigma", "tau_u", "tau_u2"] ))
+
+az.plot_trace(fit, var_names = ["alpha", "beta", "sigma", "tau_u", "tau_u2"])
+az.plot_rank(fit, var_names = ["alpha", "beta", "sigma", "tau_u", "tau_u2"])
+az.plot_forest(fit, var_names="u2_slope", combined= True, hdi_prob=.95)
+plt.show()
